1a97712b30382c9351d9db6d2247c974da52790d
ANDREW <andrew.delange@sudosky.com>
2023-06-10 12:49:18 +0200

    -Publish

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+        return cache.addAll([
+          '/close.png',
+          '/download.png',
+          '/explore.mp4',
+          '/facebook.png',
+          'logo.jpg',
+          'logo.png',
+          'logoblack.png',
+          'Logowhite.png',
+          'main.css',
+          'manifest.json',
+          'menu.svg',
+          'style.css',
+          'x.svg'
+          // Add more assets to cache as needed
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    if (event.request.url.endsWith('/index.html')) {
+      event.respondWith(
+        caches.match(event.request).then(function(response) {
+          return response || fetch(event.request).then(function(fetchResponse) {
+            var clonedResponse = fetchResponse.clone();
+            caches.open('my-cache').then(function(cache) {
+              cache.put(event.request, clonedResponse);
+            });
+            return fetchResponse;
+          });
+        })
+      );
+    }
+    // ... Handle other requests for assets, stylesheets, images, etc.
+  });
